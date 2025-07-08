744b18b9d06b143bf3f131232a9c5fb02bbd4490
muhamm111 <muhammadaminulugbekov20@gmail.com>
2025-07-08 11:03:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,413 @@
+const products = [
+
+    // MaksiBoks----<<<<<
+    {
+        id:1,
+        mazz:"Макси Бокс",
+        name:"Макси Бокс Традиция",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F78199004-19e7-479a-bfbc-247629a015e8&w=1920&q=75",
+        price: 32000,
+        category:"maksibox",
+        count: 1.
+    },
+    {
+        id:2,
+        name:"Макси Бокс Популярный",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Feb0f64ba-e9f4-4373-863c-02a4620013f7&w=1920&q=75",
+        price: 35000,
+        category:"maksibox",
+        count: 1.
+    },
+    {
+        id:3,
+        name:"Макси бокс Ретро",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fd78ad5ad-9634-4502-8e3d-f0c96d0aa323&w=1920&q=75",
+        price: 35000,
+        category:"maksibox",
+        count: 1.
+    },
+    {
+        id:4,
+        name:"Макси Бокс Тренд",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F7c408517-033f-41f9-a563-3d56410e001a&w=1920&q=75",
+        price: 30000,
+        category:"maksibox",
+        count: 1.
+    },
+
+       // Klab-sendvich----<<<<<
+
+    {
+        id:5,
+        name:"Клаб Сэндвич куриный",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F1c9ddfa3-0208-4744-896a-05cf01b292cb&w=1920&q=75",
+        price: 25000,
+        category:"sendvich",
+        count: 1.
+    },
+    {
+        id:6,
+        name:"Сэндвич Classic",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fd5306c3e-c2d9-4a51-980d-94b5dd0736cd&w=1920&q=75",
+        price: 5000,
+        category:"sendvich",
+        count: 1.
+    },
+    {
+        id:7,
+        name:"Клаб Сэндвич куриный",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fd5306c3e-c2d9-4a51-980d-94b5dd0736cd&w=1920&q=75",
+        price: 29000,
+        category:"sendvich",
+        count: 1.
+    },
+    {
+        id:8,
+        name:"Сэндвич Classic",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F1c9ddfa3-0208-4744-896a-05cf01b292cb&w=1920&q=75",
+        price: 15000,
+        category:"sendvich",
+        count: 1.
+    },
+    //    Lavash----<<<<<
+    {
+        id:9,
+        name:"Лаваш",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F7c6ef844-be8a-4b89-bed0-01841f744594&w=1920&q=75",
+        price: 25000,
+        category:"lavash",
+        count: 1.
+    },
+    {
+        id:10,
+        name:"Лаваш мини",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F607d839e-4912-4809-a90c-cc125f555851&w=1920&q=75",
+        price: 5000,
+        category:"lavash",
+        count: 1.
+    },
+    {
+        id:11,
+        name:"Лаваш куриный",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F7c6ef844-be8a-4b89-bed0-01841f744594&w=1920&q=75",
+        price: 29000,
+        category:"lavash",
+        count: 1.
+    },
+    {
+        id:12,
+        name:"Лаваш куриный",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F7c6ef844-be8a-4b89-bed0-01841f744594&w=1920&q=75",
+        price: 11500,
+        category:"lavash",
+        count: 1.
+    },
+
+    //    Shaurma----<<<<
+
+     {
+        id:13,
+        name:"Шаурма стандарт",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F457713b8-d6d5-4a83-b59e-b85a5b7641cf&w=1920&q=75",
+        price: 12000,
+        category:"shaurma",
+        count: 1.
+    },
+    {
+        id:14,
+        name:"Шаурма мясная мини",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F457713b8-d6d5-4a83-b59e-b85a5b7641cf&w=1920&q=75",
+        price: 17000,
+        category:"shaurma",
+        count: 1.
+    },
+    {
+        id:15,
+        name:"Шаурма стандарт",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F457713b8-d6d5-4a83-b59e-b85a5b7641cf&w=1920&q=75",
+        price: 12000,
+        category:"shaurma",
+        count: 1.
+    },
+    {
+        id:16,
+        name:"Шаурма мясная мини",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F457713b8-d6d5-4a83-b59e-b85a5b7641cf&w=1920&q=75",
+        price: 17000,
+        category:"shaurma",
+        count: 1.
+    },
+
+
+    //    Burger----<<<<
+
+    {
+        id:17,
+        name:"Гамбургер",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F09720aed-7d55-48ac-9c7a-17284ec04fda&w=1920&q=75",
+        price: 25000,
+        category:"burger",
+        count: 1.
+    },
+    {
+        id:18,
+        name:"Чизбургер",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F6f1d2179-2102-4462-9528-9f419aa6fdc1&w=1920&q=75",
+        price: 23000,
+        category:"burger",
+        count: 1.
+    },
+    {
+        id:19,
+        name:"Бигмакс",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F10212e4a-f4e1-4a6a-b408-29fa71348aa8&w=1920&q=75",
+        price: 32000,
+        category:"burger",
+        count: 1.
+    },
+    {
+        id:20,
+        name:"Бигмакс",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F10212e4a-f4e1-4a6a-b408-29fa71348aa8&w=1920&q=75",
+        price: 32000,
+        category:"burger",
+        count: 1.
+    },
+
+
+    //    XOt-Dog----<<<<
+
+    {
+        id:21,
+        name:"Хот Дог",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Ff2d18c0f-a9b0-4891-b252-667e07e8cb13&w=1920&q=75",
+        price: 15000,
+        category:"hotdog",
+        count: 1.
+    },
+    {
+        id:22,
+        name:"Лонгер",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Ff2d18c0f-a9b0-4891-b252-667e07e8cb13&w=1920&q=75",
+        price: 13000,
+        category:"hotdog",
+        count: 1.
+    },
+    {
+        id:23,
+        name:"Кинг Дог",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F38221f8b-98d9-4842-a41e-aa2f96ff6b69&w=1920&q=75",
+        price: 12000,
+        category:"hotdog",
+        count: 1.
+    },
+    {
+        id:24,
+        name:"Кинг- Дог",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F38221f8b-98d9-4842-a41e-aa2f96ff6b69&w=1920&q=75",
+        price: 15000,
+        category:"hotdog",
+        count: 1.
+    },
+    {
+        id:25,
+        name:"Донар Кебаб мясной",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F06568711-bb03-4bae-91ef-5067466d1e47&w=1920&q=75",
+        price: 13000,
+        category:"donar",
+        count: 1.
+    },
+ 
+    {
+        id:26,
+        name:"Шаурма баггет мини",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F59a0af1d-1763-4ab8-b938-4fa279bc96be&w=1920&q=75",
+        price: 25000,
+        category:"bagget",
+        count: 1.
+    },
+    {
+        id:27,
+        name:"Бокс",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fe4d7c6da-5913-4142-9f37-0cf52a7aed54&w=1920&q=75",
+        price: 13000,
+        category:"snek",
+        count: 1.
+    },
+    {
+        id:29,
+        name:"Картофель Фри",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fe4d7c6da-5913-4142-9f37-0cf52a7aed54&w=1920&q=75",
+        price: 15000,
+        category:"snek",
+        count: 1.
+    },
+    {
+        id:30,
+        name:"Стрипсы",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fa730d910-0f1a-476c-aa6b-3b16d2cb494b&w=1920&q=75",
+        price: 25000,
+        category:"snek",
+        count: 1.
+    },
+    {
+        id:31,
+        name:"Стрипсы",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fa730d910-0f1a-476c-aa6b-3b16d2cb494b&w=1920&q=75",
+        price: 23000,
+        category:"snek",
+        count: 1.
+    },
+
+    {
+        id:32,
+        name:"Bonaqua",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fa83a7bf1-a51a-4116-8102-4c34130068ca&w=1920&q=75",
+        price: 3000,
+        category:"napitka",
+        count: 1.
+    },
+    {
+        id:33,
+        name:"Соки Dena",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fdff23b54-ace5-4167-855e-84904c6655e4&w=1920&q=75",
+        price: 3000,
+        category:"napitka",
+        count: 1.
+    },
+    {
+        id:34,
+        name:"Айс-ти",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F08c048af-1608-4ebc-8e13-eb5ec6c94b5c&w=1920&q=75",
+        price: 9000,
+        category:"napitka",
+        count: 1.
+    },
+    {
+        id:35,
+        name:"Кола",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fac939643-a5f8-45b4-8cec-8a79d650d17f&w=1920&q=75",
+        price: 9000,
+        category:"napitka",
+        count: 1.
+    },
+    {
+        id:36,
+        name:"Чай черный",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F92aa0671-c020-444f-8fc8-ed34c37b3056&w=1920&q=75",
+        price: 3000,
+        category:"napitka",
+        count: 1.
+    },
+    {
+        id:37,
+        name:"Чай зеленый",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F92aa0671-c020-444f-8fc8-ed34c37b3056&w=1920&q=75",
+        price: 5000,
+        category:"napitka",
+        count: 1.
+    },
+    {
+        id:38,
+        name:"Разлив кола",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F20851ace-667e-4ec9-bac5-53e5e4aafe65&w=1920&q=75",
+        price: 6000,
+        category:"napitka",
+        count: 1.
+    },
+    {
+        id:39,
+        name:"Чай с лимоном",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F3773bc04-987e-46be-a094-b6bf19bb6b74&w=1920&q=75",
+        price: 6000,
+        category:"napitka",
+        count: 1.
+    },
+    {
+        id:40,
+        name:"Донат",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F6a0799c3-5594-4976-a87c-52dc50a1d960&w=1920&q=75",
+        price: 15000,
+        category:"desert",
+        count: 1.
+    },
+    {
+        id:41,
+        name:"Кетчуп",
+        image:"https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2Fcb76d569-235c-4732-a59f-2d232dd1c7be&w=1920&q=75",
+        price: 2000,
+        category:"sous",
+        count: 1.
+    },
+    
+]    
+
+let wrapper = document.querySelector('.wrapper')
+const mainCard = document.querySelector('.main-card')
+let navbar = document.createElement('nav')
+navbar.innerHTML = `    
+
+<div class="navbar bg-base-100 shadow-sm">
+<div class="flex-1">
+<a class="btn btn-ghost text-x1">daisyUI</a>
+</div>
+<div class="flex gap-2">
+<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" /> <div class="dropdown dropdown-end">
+<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+<div class="w-10 rounded-full">
+<img
+alt="Tailwind CSS Navbar component"
+src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+</div>
+</div>
+<ul
+tabindex="0"
+class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+<li>
+<a class="justify-between">
+Profile
+<span class="badge">New</span>
+</a>
+<li><a>Settings</a></li>
+</li>
+<li><a>Logout</a></li>
+</ul>
+</div>
+</div>
+</div>
+`
+
+
+wrapper.append(navbar)
+products.forEach (product => {
+console.log(products);
+let div = document.createElement('div')
+div.classList.add('card')
+div.innerHTML = `
+<div class="card bg-base-100 w-96 shadow-sm">
+<figure>
+<img
+src="${product.image}"
+alt="Image is not found" />
+</figure>
+<div class="card-body">
+<h2 class="card-title">${product.name}</h2>
+<p> price ${product.price}$</p>
+<p> category: ${product.category} </p>
+<div class="flex gap-2">
+<span class="${product.count ? 'text-success': 'text-error'}'>${product.count ? 'in Stock' : 'Low stock'}</span> 
+<span>${product.count ? product.count : ''}</span>
+</div>
+<div class="card-actions justify-end">
+<button class="btn btn-primary">Buy Now</button>
+</div>
+</div> 
+</div>
+`
+
+mainCard.append(div)
+
+
+});
+
+console.log(products);
